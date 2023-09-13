84ed5738b8494fdd05cbbeb321473b12b642f92a
Peter-The-Great <PjotrW15@Gmail.com>
2023-09-13 11:27:16 +0200

    Programma Werkt

--- Async-Opdracht/Program.cs
+++ Async-Opdracht/Program.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-
-namespace AsyncBoekOpdracht
+﻿namespace AsyncBoekOpdracht
 {
-    class Boek
+    public class Boek
     {
-        public string Titel { get; set; }
-        public string Auteur { get; set; }
+        public string? Titel { get; set; }
+        public string? Auteur { get; set; }
         public float AIScore
         {
             get
@@ -22,7 +18,7 @@ namespace AsyncBoekOpdracht
             }
         }
     }
-    static class Willekeurig
+    public static class Willekeurig
     {
         public static Random Random = new Random();
         public static async Task Vertraging(int MinAantalMs = 500, int MaxAantalMs = 1000)
@@ -30,7 +26,7 @@ namespace AsyncBoekOpdracht
             await Task.Delay(Random.Next(MinAantalMs, MaxAantalMs));
         }
     }
-    static class Database
+    public static class Database
     {
         private static List<Boek> lijst = new List<Boek>();
         public static async void VoegToe(Boek b)
@@ -48,9 +44,9 @@ namespace AsyncBoekOpdracht
             await Willekeurig.Vertraging(); // schrijf naar logbestand
         }
     }
-    class Program
+    public class Program
     {
-        static void VoegBoekToe()
+        static async void VoegBoekToe()
         {
             Console.WriteLine("Geef de titel op: ");
             var titel = Console.ReadLine();
@@ -59,21 +55,35 @@ namespace AsyncBoekOpdracht
             Database.VoegToe(new Boek { Titel = titel, Auteur = auteur });
             Database.Logboek("Er is een nieuw boek!");
             Console.WriteLine("De huidige lijst met boeken is: ");
-            foreach (var boek in Database.HaalLijstOp())
+            foreach (var boek in await Database.HaalLijstOp())
             {
                 Console.WriteLine(boek.Titel);
             }
         }
-        static void ZoekBoek()
+        static async Task ZoekBoek()
         {
             Console.WriteLine("Waar gaat het boek over?");
             var beschrijving = Console.ReadLine();
-            Boek beste = null;
-            foreach (var boek in Database.HaalLijstOp())
-                if (beste == null || boek.AIScore > beste.AIScore)
-                    beste = boek;
-            Console.WriteLine("Het boek dat het beste overeenkomt met de beschrijving is: ");
-            Console.WriteLine(beste.Titel);
+            Boek? beste = null;
+            var boeken = await Database.HaalLijstOp();
+            if (boeken != null && boeken.Count > 0)
+            {
+                beste = boeken[0]; // Initialiseren met eerste boek
+                foreach (var boek in boeken)
+                {
+                    if (beste == null || boek.AIScore > beste.AIScore)
+                        beste = boek;
+                }
+            }
+            if (beste != null)
+            {
+                Console.WriteLine("Het boek dat het beste overeenkomt met de beschrijving is: ");
+                Console.WriteLine(beste.Titel);
+            }
+            else
+            {
+                Console.WriteLine("Er zijn geen boeken gevonden die overeenkomen met de beschrijving.");
+            }
         }
         static bool Backupping = false;
         // "Backup" kan lang duren. We willen dat de gebruiker niet hoeft te wachten,
@@ -85,11 +95,13 @@ namespace AsyncBoekOpdracht
             Backupping = true;
             await Willekeurig.Vertraging(2000, 3000);
             Backupping = false;
+            Console.WriteLine("Backup is gemaakt.");
         }
-        static void Main(string[] args)
+
+        public static async Task Main(string[] args)
         {
             Console.WriteLine("Welkom bij de boeken administratie!");
-            string key = null;
+            string? key = null;
             while (key != "q")
             {
                 Console.WriteLine("+) Boek toevoegen");
@@ -100,7 +112,7 @@ namespace AsyncBoekOpdracht
                 if (key == "+")
                     VoegBoekToe();
                 else if (key == "z")
-                    ZoekBoek();
+                    await ZoekBoek();
                 else if (key == "b")
                     await Backup();
                 else Console.WriteLine("Ongeldige invoer!");
